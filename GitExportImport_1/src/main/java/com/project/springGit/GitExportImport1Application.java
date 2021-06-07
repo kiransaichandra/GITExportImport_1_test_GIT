fbaf81c5a7a0da16d801d414198e346647eceb1a
KiranDulam <kiransaichandra@gmail.com>
2021-06-07 00:20:52 -0400

    FirstGitExportCommit

---
+++ GitExportImport_1/src/main/java/com/project/springGit/GitExportImport1Application.java
@@ -0,0 +1,13 @@
+package com.project.springGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExportImport1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExportImport1Application.class, args);
+	}
+
+}
---
+++ GitExportImport_1/src/test/java/com/project/springGit/GitExportImport1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExportImport1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
